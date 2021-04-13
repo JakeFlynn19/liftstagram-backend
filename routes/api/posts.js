adcaec8f4ee236c26ac5bc2d8e8092d3b6a3962c
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-04-13 10:12:04 -0400

    wip: mvp

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,5 +4,7 @@ const postsCtrl = require('../../controllers/posts');
 
 router.get("/", postsCtrl.index);
 router.post("/", postsCtrl.create);
+router.delete('/:id', postsCtrl.delete);
+router.put('/:id', postsCtrl.update);
 
 module.exports = router;
